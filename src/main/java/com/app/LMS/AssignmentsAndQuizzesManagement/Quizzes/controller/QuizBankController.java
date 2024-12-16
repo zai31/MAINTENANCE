cd26a75f9d5d294da52c07a3198c940155544615
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2024-12-17 00:58:17 +0200

    Update QuizBankController.java

--- src/main/java/com/app/LMS/AssignmentsAndQuizzesManagement/Quizzes/controller/QuizBankController.java
+++ src/main/java/com/app/LMS/AssignmentsAndQuizzesManagement/Quizzes/controller/QuizBankController.java
@@ -1,7 +1,6 @@
 package com.app.LMS.AssignmentsAndQuizzesManagement.Quizzes.controller;
 
 import com.app.LMS.AssignmentsAndQuizzesManagement.Quizzes.service.QuizBankService;
-import com.app.LMS.AssignmentsAndQuizzesManagement.Quizzes.service.QuizService;
 import com.app.LMS.DTO.QuizBankRequest;
 import com.app.LMS.DTO.QuizFromBankRequest;
 import org.springframework.http.HttpStatus;
@@ -32,13 +31,13 @@ public class QuizBankController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
-    @PutMapping("/{courseId}/quizzes/quizBank/{quizBankId}")
-    public ResponseEntity<String> editQuizBank(@RequestHeader("Authorization") String token, @PathVariable Long courseId, @RequestBody QuizBankRequest quizBangRequest ,@PathVariable Long quizBankId) {
+    @PutMapping("/{courseId}/quizzes/quizBank")
+    public ResponseEntity<String> editQuizBank(@RequestHeader("Authorization") String token, @PathVariable Long courseId, @RequestBody QuizBankRequest quizBangRequest ) {
         try {
-            ResponseEntity<String> error = quizBankService.validation(token,courseId,quizBankId,quizBangRequest.getQuestions());
+            ResponseEntity<String> error = quizBankService.validation(token,courseId,quizBangRequest.getQuestions());
             if (error == null)
             {
-                return quizBankService.editQuizBank(quizBankId,quizBangRequest);
+                return quizBankService.editQuizBank(courseId,quizBangRequest);
             }
             else {
                 return error;
@@ -47,14 +46,14 @@ public class QuizBankController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
-    @DeleteMapping("/{courseId}/quizzes/quizBank/{quizBankId}")
-    public ResponseEntity<String> deleteQuizBank(@RequestHeader("Authorization") String token, @PathVariable Long courseId,@PathVariable Long quizBankId) {
+    @DeleteMapping("/{courseId}/quizzes/quizBank")
+    public ResponseEntity<String> deleteQuizBank(@RequestHeader("Authorization") String token, @PathVariable Long courseId) {
         try
         {
-            ResponseEntity<String> error = quizBankService.validation(token,courseId,quizBankId);
+            ResponseEntity<String> error = quizBankService.validation(token,courseId);
             if (error == null)
             {
-                return quizBankService.deleteQuizBank(quizBankId);
+                return quizBankService.deleteQuizBank(courseId);
             }
             else {
                 return error;
@@ -64,14 +63,14 @@ public class QuizBankController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
-    @PostMapping("/{courseId}/quizzes/quizBank/{quizBankId}/createQuiz")
-    public ResponseEntity<String> createQuizFromQuizBank(@RequestHeader("Authorization") String token, @PathVariable Long courseId, @PathVariable Long quizBankId,@RequestBody QuizFromBankRequest request) {
+    @PostMapping("/{courseId}/quizzes/quizBank/createQuiz")
+    public ResponseEntity<String> createQuizFromQuizBank(@RequestHeader("Authorization") String token, @PathVariable Long courseId,@RequestBody QuizFromBankRequest request) {
         try
         {
-            ResponseEntity<String> error = quizBankService.validation(token,courseId,quizBankId);
+            ResponseEntity<String> error = quizBankService.validation(token,courseId);
             if (error == null)
             {
-                return quizBankService.createQuizFromBankQuiz(request,courseId,quizBankId);
+                return quizBankService.createQuizFromBankQuiz(request,courseId);
             }
             else {
                 return error;
