6be98e11373e76b9e59b59ab94f5f3a9416e48c9
user <noranehab414@gmail.com>
2025-05-04 18:00:30 +0300

    n

---
+++ MAINTENANCE-main/src/main/java/com/app/LMS/LmsApplication.java
@@ -0,0 +1,17 @@
+package com.app.LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class LmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsApplication.class, args);
+	}
+
+}
+/////test
+/////test
